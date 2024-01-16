ed85005a1a1b7ebb89d77899047bc2a9e5184747
Willie Ku <willieku123@gmail.com>
2024-01-16 16:54:32 +0800

    clean up & add readme

--- README.md
+++ README.md
@@ -1,3 +1,52 @@
-# ascii-art-generator
+# ASCII Art Generator
 
-TBD
+![星期一清大沒有停電，交又贏](assets/nthu.jpg "星期一清大沒有停電，交又贏")
+
+
+## Prepare Environment
+
+Create a conda environment:
+
+```
+conda create -n ascii_art python=3.8
+conda activate ascii_art
+pip install -r requirements.txt
+```
+
+## Generate ASCII Art
+
+Generate aN ASCII Art iamge:
+
+```
+python text2image.py --path "input/wiki.png"
+```
+
+the generated image and txt file will be saved at output/ .
+
+
+Generate ASCII Art iamge in Madarin, composed of custom charactors , in dark mode, and in different resolution:
+```
+python text2image.py --path "input/nthu.jpg" --language "mandarin" --char "星期一清大沒有停電，交又贏" --dark --size_factor 15
+```
+
+```
+                 ,       _,   /;                
+               ," ',     ; - "`^                
+              ;`    ""y`                        
+              !.,   ,'   *      ,               
+             ! `"-x.'           "\              
+            !      L     !       "~,            
+           /       !     !          *           
+           `       "<     \          L          
+          "          ,    "          !          
+         "           L     ,         "          
+         *           "     !         '          
+ ~.~..~~.,.l._.*Z~.,(.~~,"T` :~,_,.~i~..~....~~ 
+             `*_"<         '"~--~~"`            
+               " "                              
+               ^ !                              
+              ! ;                               
+              ! `                               
+              ! !~:*                            
+               ".-'"
+```
--- text2image.py
+++ text2image.py
@@ -1,11 +1,11 @@
 # coding:utf-8
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
-import cupy as np
-import numpy as real_np
+import numpy as np
 import string
 from scipy import signal
 import time
 import argparse
+import os
 
 
 def textsize(text, font):
@@ -15,14 +15,14 @@ def textsize(text, font):
     return width, height
 
 
-def text_to_image(input_text, image_size, dark, language):
+def text_to_image(input_text, image_size, dark, language, font_size=9):
     image = Image.new("RGB", image_size, "black" if dark else "white")
     draw = ImageDraw.Draw(image)
     
     if language == "mandarin":
-        font = ImageFont.truetype("mingliu.ttc", 9)
+        font = ImageFont.truetype("mingliu.ttc", font_size)
     elif language == "english":
-        font = ImageFont.truetype("consola.ttf", 11)
+        font = ImageFont.truetype("consola.ttf", font_size)
     text_width, text_height = textsize("哈", font)
     
     x = (image_size[0] - text_width) // 2
@@ -36,55 +36,70 @@ def text_to_image(input_text, image_size, dark, language):
 
 
 def show_image(img_np, bias=False):
-    a = Image.fromarray(real_np.array(img_np.get()))
+    a = Image.fromarray(np.array(img_np))
     a.show(a)
 
 
 def remap_by_occurrence(arr):
-    arr = real_np.asarray(arr.get())
     # Count occurrences of each integer in the array
-    unique_values, counts = real_np.unique(arr, return_counts=True)
+    unique_values, counts = np.unique(arr, return_counts=True)
 
     # Sort based on occurrences
-    sorted_indices = real_np.argsort(counts)[::-1]
+    sorted_indices = np.argsort(counts)[::-1]
     sorted_values = unique_values[sorted_indices]
 
     # Create a mapping dictionary based on sorted order
     remap_dict = {value: i for i, value in enumerate(sorted_values)}
 
     # Use the mapping to remap the array
-    remapped_arr = real_np.vectorize(remap_dict.get)(arr)
+    remapped_arr = np.vectorize(remap_dict.get)(arr)
 
-    return np.asarray(remapped_arr)
+    return remapped_arr
 
+
+def get_loss_map(target_img, char_img_list):
+    char_max = max([i.mean() for i in char_img_list])
+    char_min = min([i.mean() for i in char_img_list])
+
+    losses = []
+
+    for char_patch in char_img_list:
+        target_img_norm = target_img / 255
+        char_patch_norm = (char_patch-char_min)/(char_max-char_min)
+        
+        # per patch mean
+        per_patch_mean = np.abs( signal.correlate2d(target_img_norm, np.ones_like(char_patch_norm), mode='same', boundary='symm') - char_patch_norm.sum())
+        
+        # per pixel MSE
+        per_pixel_MSE =  - signal.correlate2d((1-target_img_norm), (1-char_patch_norm), mode='same', boundary='symm') - signal.correlate2d(target_img_norm, char_patch_norm, mode='same', boundary='symm')
+        
+        loss = per_patch_mean + per_pixel_MSE * 2
+
+        losses.append(loss)
+
+    return np.stack(losses, axis=0)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--path', type=str, default="wiki.png")  #, required=True
-    parser.add_argument('--char', type=str, default=string.printable[:-5])
+    parser.add_argument('--path', type=str, default="input/cats.jpg")  #, required=True
+
+    parser.add_argument('--output_path', type=str, default="output/output.png")
+    parser.add_argument('--output_txt', type=str, default="output/output.txt")
 
     parser.add_argument('--language', type=str, default="english", choices=["english", "mandarin"])
+    parser.add_argument('--char', type=str, default=string.printable[:-5])
     parser.add_argument('--dark', action="store_true")
     parser.add_argument('--size_factor', type=int, default=8)
+    parser.add_argument('--output_size_factor', type=int, default=2)
     args = parser.parse_args()
 
 
-    #args.char = 
-    #args.char = " ASCIRT"
-    #args.char = "星期一清大沒有停電，交又贏"
-    #args.path = 'image3.jpg'
-    #args.path = 'image4.png'
-    #args.path = 'image6.jpg'
-    #args.path = 'ml.jpg'
-    #args.path = 'ascii_art.png'
+    start_time = time.time()
 
 
-
-    # get char list
-    char_list = [i for i in args.char]
-
+    # get list of char image patches
     if args.language == "mandarin":
         PATCH_H = 9
         PATCH_W = 9
@@ -94,50 +109,30 @@ if __name__ == '__main__':
     else:
         raise NotImplementedError(f"'{args.language}' language not implemented")
 
-
-    # get normalized char image patches
+    char_list = [i for i in args.char]
     char_img_list = [text_to_image(char,(PATCH_W,PATCH_H), args.dark, args.language)[..., 0] for char in char_list]
-    b = np.tile(np.concatenate(char_img_list, axis=1), (1, 2, 1))
-    char_max = max([i.mean() for i in char_img_list])
-    char_min = min([i.mean() for i in char_img_list])
+    char_img_list_big = [text_to_image(char, (PATCH_W * args.output_size_factor, PATCH_H * args.output_size_factor), args.dark, args.language, 9 * args.output_size_factor)[..., 0] for char in char_list]
+    print(f"Char list: {args.char}")    
 
-    # get normalized target image
+
+    # get resized target image
     img = Image.open(args.path).convert('L')
     img = img.resize((PATCH_H*PATCH_W*args.size_factor, PATCH_H*(PATCH_W*args.size_factor * img.size[1] // img.size[0]) ))#.filter(ImageFilter.FIND_EDGES)
-    img.show(img)
     target_img = np.array(img).astype(float)
+    
+    output_img = np.zeros_like(np.tile(target_img, (args.output_size_factor, args.output_size_factor)))
 
 
-    # get closest char patches for each target image patches
-    H, W = target_img.shape
-    H = H // PATCH_H
-    W = W // PATCH_W
 
-    output_img = np.zeros_like(target_img)
-
-    #'''
-    start_time = time.time()
-    losses = []
-    for char_patch in char_img_list:
-        target_img_norm = target_img / 255
-        char_patch_norm = (char_patch-char_min)/(char_max-char_min)
-        unit_mean = np.ones_like(char_patch_norm) * char_patch_norm.mean()
-        
-        # per patch mean
-        per_patch_mean = real_np.abs( signal.correlate2d(target_img_norm.get(), np.ones_like(char_patch_norm).get(), mode='same', boundary='symm') - char_patch_norm.sum().get())
-        
-        # per pixel MSE
-        per_pixel_MSE =  - signal.correlate2d((1-target_img_norm).get(), (1-char_patch_norm).get(), mode='same', boundary='symm') - signal.correlate2d(target_img_norm.get(), char_patch_norm.get(), mode='same', boundary='symm')
-        
-        loss = per_patch_mean + per_pixel_MSE * 2
-
-        losses.append(np.asarray(loss))
-    loss_map = np.stack(losses, axis=0)
+    loss_map = get_loss_map(target_img, char_img_list)
     decision_map = np.argmin(loss_map, axis=0)
 
+    if not os.path.exists(os.path.dirname(args.output_txt)):
+        os.makedirs(os.path.dirname(args.output_txt))
+    f = open(args.output_txt, 'w')
 
-    for i in range(H):
-        for j in range(W):
+    for i in range(target_img.shape[0] // PATCH_H):
+        for j in range(target_img.shape[1] // PATCH_W):
             H_start = i*PATCH_H + PATCH_H //3
             H_end = (i+1)*PATCH_H - PATCH_H //3
             W_start = j*PATCH_W + PATCH_W //3
@@ -145,11 +140,15 @@ if __name__ == '__main__':
 
             decision = decision_map[H_start : H_end, W_start : W_end]
             occurence = remap_by_occurrence(decision).flatten()
-            decision = decision.flatten()[np.argmax(occurence)]
+            decision = int(decision.flatten()[np.argmax(occurence)])
 
-            output_img[i*PATCH_H : (i+1)*PATCH_H, j*PATCH_W : (j+1)*PATCH_W] = char_img_list[int(decision)]
+            f.write(args.char[decision])
+            output_img[i*PATCH_H*args.output_size_factor : (i+1)*PATCH_H*args.output_size_factor, j*PATCH_W*args.output_size_factor : (j+1)*PATCH_W*args.output_size_factor] = char_img_list_big[decision]
+        f.write('\n')
+    f.close()
+
+    out = Image.fromarray(output_img.astype(np.uint8))
+    out.save(args.output_path)
+    #show_image(out)
+
     print(f"elapsed time {time.time() - start_time} seconds.")
-    #'''
-
-
-    show_image(output_img)
